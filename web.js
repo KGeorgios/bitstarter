09ced35e7eb1fd5dbb71dd9929cee711f789470b
KGeorgios <gkoutep@netmode.ntua.gr>
2013-07-21 15:37:04 +0000

    Trying web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var mybuffer = new Buffer(100);
+var stringlength=mybuffer.write(fs.readFileSync("index.html", 'utf8'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(mybuffer.toString('utf8', 0, stringlength));
 });
 
 var port = process.env.PORT || 5000;
