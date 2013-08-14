0c892840fcecac27ccbb511a8f484049446348ae
KGeorgios <gkoutep@netmode.ntua.gr>
2013-08-14 14:01:20 +0300

    renames

--- grader.js
+++ grader.js
@@ -29,9 +29,6 @@ var rest = require('restler');
 
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URLFILE_DEFAULT = "http://127.0.0.1:5000";
-
-var generalbuffer;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -42,92 +39,70 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var checkHtmlFile = function(data, checkfile, is_url) {
+    if (!is_url) {
+      $ = cheerioHtmlFile(data);
+    }
+    else {
+      $=cheerio.load(data);
+    }
+    var checks = loadChecks(checkfile).sort();
+    
+    var out={};
+
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
-    }
-    return out;
-};
+        }
+    return out;    
+}
 
-var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
-    return fn.bind({});
-};
 
-//--- My "own" code starts here --
-
-/*var checkurl = function (url) {
-  var instr = url.toString();
-  generalbuffer = new Buffer(requestURL(instr), 'utf8');
-}*/
-
-var requestURL = function(url, checksfile) {    
-  console.log("POINT requestURL");
-  rest.get(url).on('complete', 
-    function(result, response) {
-      if (result instanceof Error) {
-          console.log("POINT requestURL - FAILURE");
+var call_url = function(url, checkfile) {
+rest.get(url).on('complete', function(data, response) {
+      if (response instanceof Error) {
           console.log("Cannot load URL %s", url);
           process.exit(1);
       } 
-      else {
-        console.log("POINT requestURL - SUCCESS");
-        console.log("Processing URL %s", url);
-        console.log("POINT 1 ", result);
-        return processURL(result, checksfile);
-      }
-    }
-    );
-console.log("POINT - PRINTING THIS WHILE WAITING");
-};
-
-var processURL = function(remotefile, checksfile) {
-    abc = cheerioHtmlFile(remotefile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    console.log("RESULT:");
-    console.log("POINT 2 ", remotefile);
-    console.log("CHECKS:");
-    console.log(checks);
-    for(var ii in checks) {
-        var present = abc(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
-};
-
+      else {    
+    checkJson = checkHtmlFile(data, checkfile, 1);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+ }
+   });
+}
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to file to check', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_to_check>', 'URL path to file to check', URLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to file to check', assertFileExists, HTMLFILE_DEFAULT)
+        .option('-u, --url <url_to_check>', 'URL path to file to check')
         .parse(process.argv);
     
-    if (program.file){
-      console.log("checking file");  
-      checkJson = checkHtmlFile(program.file, program.checks)
-    }
+    // Default values turn program.file always true
+
     if (program.url){
-      console.log("checking URL");
-      checkJson = requestURL(program.url, program.checks)};
-var outJson = JSON.stringify(checkJson, null, 4);
-console.log(outJson);
-} 
+      console.log("Checking URL: ", program.url);
+      call_url(program.url, program.checks);
+      console.log("Waiting while we retrieve URL...\n\n");      
+    }
+
+    else {
+      console.log("Checking file: ", program.file);  
+      checkJson = checkHtmlFile(program.file, program.checks, 0)
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
+}     
 else {
     exports.checkHtmlFile = checkHtmlFile;
-    exports.requestURL = requestURL;
+    exports.call_url = call_url;
 }
